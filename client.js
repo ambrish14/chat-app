bb7dacfbc4988cb9c23ecffaedce6257af5358b1
Ambrish Chaurasia <69149051+ambrish14@users.noreply.github.com>
2026-06-24 16:07:02 +0530

    adding the feature ask and clearLine function

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+const { rejects } = require("assert");
+const { resolve } = require("dns");
 const net = require("net");
 const readline = require("readline/promises");
 
@@ -5,15 +7,31 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+const clearLine = (dir) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+};
 const client = net.createConnection(
   { host: "127.0.0.1", port: 3008 },
   async () => {
     console.log("Connected to the server ");
-
-    const message = await rl.question("Enter a message >");
-    client.write(message);
+    const ask = async () => {
+      const message = await rl.question("Enter a message >");
+      // clear the  current line that the cursor in
+      await clearLine(0);
+      client.write(message);
+    };
+    ask();
   },
 );
+
+client.on("data", (data) => {
+  console.log(data.toString("utf-8"));
+});
 
 client.on("end", () => {
   console.log("connectio was ended!");
