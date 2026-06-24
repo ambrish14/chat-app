0fd4ea69619530a342dd58d91e21703df25cfce9
Ambrish Chaurasia <69149051+ambrish14@users.noreply.github.com>
2026-06-24 10:20:17 +0530

    here we printing the data from the  connected  client by using data event  and priting to the console

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const net = require("net");
 const server = net.createServer();
 server.on("connection", (Socket) => {
   console.log("a new connection to the server");
+
+  Socket.on("data", (data) => {
+    console.log(data.toString("utf-8"));
+  });
 });
 
 server.listen(3008, "127.0.01", () => {
