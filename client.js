c197d70f4afc99a61b3b808e5b92862a74d1460f
Ambrish Chaurasia <69149051+ambrish14@users.noreply.github.com>
2026-06-24 10:18:48 +0530

    here we are using readline to module to take user input and  writing to the server

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+const readline = require("readline/promises");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const client = net.createConnection(
+  { host: "127.0.0.1", port: 3008 },
+  async () => {
+    console.log("Connected to the server ");
+
+    const message = await rl.question("Enter a message >");
+    client.write(message);
+  },
+);
+
+client.on("end", () => {
+  console.log("connectio was ended!");
+});
